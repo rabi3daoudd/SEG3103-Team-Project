0ac526010aa8cb4357f21920f768a5d3c76ff0be
rabi3daoudd <rmura096@uottawa.ca>
2022-07-16 11:32:48 -0400

    Testing file

---
+++ MainTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void reverse1() {
+    }
+
+    @org.junit.Test
+    public void reverse2() {
+    }
+
+    @org.junit.Test
+    public void reverse3() {
+    }
+
+    @org.junit.Test
+    public void reverse4() {
+    }
+
+    @org.junit.Test
+    public void reverse5() {
+    }
+}
